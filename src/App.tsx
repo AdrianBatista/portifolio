e3ba1f3764f9977931bef75be4174ebceacf751e
Adrian Batista <adrianfnb@gmail.com>
2023-06-21 16:58:11 -0300

    mock first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,61 @@
+import ResponsiveAppBar from "./components/interface/ResponsibleAppBar";
+import "./App.css";
+import { Paper, Typography } from "@mui/material";
+
 export default function App() {
-  return <>Hello Word</>;
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <Paper
+        elevation={0}
+        sx={{
+          backgroundImage: "url('/images/pc.jpg')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Paper
+          elevation={0}
+          sx={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.8)",
+          }}
+        >
+          <Typography variant="h3" sx={{ color: "#DDD" }}>
+            Adrian Batista
+          </Typography>
+        </Paper>
+      </Paper>
+      <Paper
+        elevation={0}
+        sx={{
+          backgroundImage: "url('/images/pc.jpg')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <Paper
+          elevation={0}
+          sx={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0,0,0,0.8)",
+          }}
+        >
+          <Typography variant="h3" sx={{ color: "#DDD" }}>
+            Adrian Batista
+          </Typography>
+        </Paper>
+      </Paper>
+    </>
+  );
 }
