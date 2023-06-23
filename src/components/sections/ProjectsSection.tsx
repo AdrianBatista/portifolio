8d659d0ad4b543e46fa866e1fe903ffde1bd25c9
Adrian Batista <adrianfnb@gmail.com>
2023-06-23 11:24:24 -0300

    see more projects button

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,5 +1,5 @@
 import Panel from "../interface/Panel";
-import { Typography, Divider, Container } from "@mui/material";
+import { Typography, Divider, Container, Button } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import Card from "../interface/Card";
 
@@ -11,7 +11,7 @@ export default function ProjectSection() {
           My Projects
         </Typography>
         <Divider />
-        <Grid container spacing={2} mt={2}>
+        <Grid container spacing={2} mt={1}>
           <Grid xs={12} sm={6} md={4} xl={3}>
             <Card />
           </Grid>
@@ -25,6 +25,15 @@ export default function ProjectSection() {
             <Card />
           </Grid>
         </Grid>
+        <Typography
+          mt={1}
+          sx={{
+            display: "flex",
+            justifyContent: { xs: "center", sm: "flex-end" },
+          }}
+        >
+          <Button href="/projects">See more &#10132;</Button>
+        </Typography>
       </Container>
     </Panel>
   );
