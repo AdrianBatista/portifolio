9224590c3b8d055b5559b321b802f9372a68c177
Adrian Batista <adrianfnb@gmail.com>
2023-06-25 13:05:25 -0300

    cover message

--- src/components/sections/CoverSection.tsx
+++ src/components/sections/CoverSection.tsx
@@ -1,12 +1,22 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Panel from "../interface/Panel";
 
 export default function CoverSection() {
   return (
     <Panel canvas={true} background={{ type: "image", url: "/images/pc.jpg" }}>
-      <Typography variant="h3" sx={{ color: "#DDD" }}>
-        Adrian Batista
-      </Typography>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography variant="h3" sx={{ color: "#DDD" }}>
+          HI! I'M <span style={{ color: "rgb(207, 255, 4)" }}>ADRIAN</span>
+        </Typography>
+        <Typography variant="h5" sx={{ color: "#DDD" }}>
+          I TRANSFORM IDEAS INTO
+        </Typography>
+        <Typography variant="h5" sx={{ color: "rgb(207, 255, 4)" }}>
+          POWERFULL SOLUTIONS
+        </Typography>
+      </Box>
     </Panel>
   );
 }
